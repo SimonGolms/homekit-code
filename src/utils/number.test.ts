0ea4123383530d2db4067a6b7ee43ef9321c103e
Simon G <development@gol.ms>
2022-01-31 20:08:01 +0100

    test(numbers): refactor iteration about expection

--- src/utils/number.test.ts
+++ src/utils/number.test.ts
@@ -3,14 +3,14 @@ import { isNumber, isPairingCode } from './number';
 describe('isNumber', () => {
   test('its a number', () => {
     expect.assertions(6);
-    ['0', '1', '2', '-1', '1.345e17', '1'].map((number) => {
+    ['0', '1', '2', '-1', '1.345e17', '1'].forEach((number) => {
       expect(isNumber(number)).toBe(true);
     });
   });
 
   test('its not a number', () => {
     expect.assertions(6);
-    ['false', 'true', 'NaN', '[]', '{}', '1a'].map((number) => {
+    ['false', 'true', 'NaN', '[]', '{}', '1a'].forEach((number) => {
       expect(isNumber(number)).toBe(false);
     });
   });
@@ -19,14 +19,14 @@ describe('isNumber', () => {
 describe('isPairingCode', () => {
   test('its a pairingCode', () => {
     expect.assertions(2);
-    ['00000000', '84131633'].map((number) => {
+    ['00000000', '84131633'].forEach((number) => {
       expect(isPairingCode(number)).toBe(true);
     });
   });
 
   test('its not a pairingCode', () => {
     expect.assertions(15);
-    ['false', 'true', 'NaN', '[]', '{}', '1a', '1', '12', '123', '1234', '12345', '123456', '1234567', '123456789', '123-45-678'].map(
+    ['false', 'true', 'NaN', '[]', '{}', '1a', '1', '12', '123', '1234', '12345', '123456', '1234567', '123456789', '123-45-678'].forEach(
       (number) => {
         expect(isPairingCode(number)).toBe(false);
       },
