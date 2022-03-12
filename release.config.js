2de11344f5ebda8248366b4bd04076335a6cab8d
Simon G <development@gol.ms>
2022-03-12 11:26:26 +0100

    ci(release): enhance the release notes
    - use conventionalcommits as preset and update extend preset config

--- release.config.js
+++ release.config.js
@@ -20,9 +20,23 @@ module.exports = {
         parserOpts: {
           noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES', 'BREAKING'],
         },
-        preset: 'angular',
+        preset: 'conventionalcommits',
         writerOpts: {
           commitsSort: ['subject', 'scope'],
+        },
+        presetConfig: {
+          types: [
+            { type: 'build', section: 'Build System', hidden: false },
+            { type: 'chore', section: 'Build System', hidden: false },
+            { type: 'ci', section: 'Continuous Integration', hidden: false },
+            { type: 'docs', section: 'Documentation', hidden: false },
+            { type: 'feat', section: 'Features', hidden: false },
+            { type: 'fix', section: 'Bug Fixes', hidden: false },
+            { type: 'perf', section: 'Performance Improvements', hidden: false },
+            { type: 'refactor', section: 'Code Refactoring', hidden: false },
+            { type: 'style', section: 'Styles', hidden: false },
+            { type: 'test', section: 'Tests', hidden: false },
+          ],
         },
       },
     ],
